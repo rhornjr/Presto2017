b1404f7305d4aeea7030d6c4a602292db7c757c6
rhornjr_cp <SND\rhornjr_cp>
2011-11-29 23:24:47 +0000

    Get UI to show data.

---
+++ Main/Solutions/Presto/Source/Client/PrestoViewModel/Tabs/ApplicationViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Globalization;
+using System.Linq;
+using Db4objects.Db4o;
+using Db4objects.Db4o.CS;
+using Db4objects.Db4o.Linq;
+using PrestoCommon.Entities;
+
+namespace PrestoViewModel.Tabs
+{
+    public class ApplicationViewModel : ViewModelBase
+    {
+        private List<Application> _applications;
+
+        public List<Application> Applications
+        {
+            get { return this._applications; }
+
+            set
+            {
+                this._applications = value;
+                this.NotifyPropertyChanged("Applications");
+            }
+        }
+
+        public ApplicationViewModel()
+        {
+            LoadApplications();
+        }
+
+        private void LoadApplications()
+        {
+            IObjectContainer db = GetDatabase();            
+
+            IEnumerable<Application> apps = from Application app in db
+                                            select app;
+
+            this.Applications = apps.ToList();
+
+            db.Close();
+        }
+
+        private static IObjectContainer GetDatabase()
+        {
+            string databaseServerName = ConfigurationManager.AppSettings["databaseServerName"];
+            string databaseUser       = ConfigurationManager.AppSettings["databaseUser"];
+            string databasePassword   = ConfigurationManager.AppSettings["databasePassword"];
+            int databaseServerPort    = Convert.ToInt32(ConfigurationManager.AppSettings["databaseServerPort"], CultureInfo.InvariantCulture);
+
+            return Db4oClientServer.OpenClient(databaseServerName, databaseServerPort, databaseUser, databasePassword);
+        }   
+    }
+}
---
+++ Main/Solutions/Presto/Source/Client/PrestoViewModel/ViewModelBase.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace PrestoViewModel
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void NotifyPropertyChanged(string propertyName)
+        {
+            if (this.PropertyChanged == null) { return; }
+
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}

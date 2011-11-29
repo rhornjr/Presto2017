f12de801e93b531c20301efeff6fd6d0dc2422f2
Bob Horn <rhornjr@gmail.com>
2011-11-29 21:19:04 +0000

    Add some entities, the dashboard client app, the task runner app, and modify some testing.

---
+++ Presto/Source/Common/PrestoCommon/Entities/Application.cs
@@ -0,0 +1,50 @@
+﻿
+using System.Collections.ObjectModel;
+namespace PrestoCommon.Entities
+{
+    /// <summary>
+    /// An application, or product, that gets installed.
+    /// </summary>
+    public class Application
+    {
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the version.
+        /// </summary>
+        /// <value>
+        /// The version.
+        /// </value>
+        public string Version { get; set; }
+
+        /// <summary>
+        /// Gets or sets the release folder location.
+        /// </summary>
+        /// <value>
+        /// The release folder location.
+        /// </value>
+        public string ReleaseFolderLocation { get; set; }
+
+        /// <summary>
+        /// Gets or sets the tasks.
+        /// </summary>
+        /// <value>
+        /// The tasks.
+        /// </value>
+        public Collection<TaskBase> Tasks { get; private set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Application"/> class.
+        /// </summary>
+        public Application()
+        {
+            this.Tasks = new Collection<TaskBase>();
+        }
+    }
+}
---
+++ Presto/Source/Common/PrestoCommon/Enums/TaskType.cs
@@ -0,0 +1,16 @@
+﻿
+namespace PrestoCommon.Enums
+{
+    /// <summary>
+    /// Task type
+    /// </summary>
+#pragma warning disable 1591
+    public enum TaskType
+    {
+        CopyFile,
+        DosCommand,
+        Installer,
+        XmlModify
+    }
+#pragma warning restore 1591
+}

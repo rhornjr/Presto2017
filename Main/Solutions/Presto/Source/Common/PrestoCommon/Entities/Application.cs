5c29e889918bed110d58c409949e1a0c4f788106
rhornjr_cp <SND\rhornjr_cp>
2011-12-03 04:46:44 +0000

    Get updating to work with db4o client/server model: use transparent persistence.

--- Main/Solutions/Presto/Source/Common/PrestoCommon/Entities/Application.cs
+++ Main/Solutions/Presto/Source/Common/PrestoCommon/Entities/Application.cs
@@ -1,12 +1,18 @@
 ﻿
 using System.Collections.ObjectModel;
+using Db4objects.Db4o;
+using Db4objects.Db4o.Activation;
+using Db4objects.Db4o.TA;
 namespace PrestoCommon.Entities
 {
     /// <summary>
     /// An application, or product, that gets installed.
     /// </summary>
-    public class Application
+    public class Application : IActivatable
     {
+        [Transient]
+        private IActivator _activator;
+
         /// <summary>
         /// Gets or sets the name.
         /// </summary>
@@ -45,6 +51,31 @@ namespace PrestoCommon.Entities
         public Application()
         {
             this.Tasks = new Collection<TaskBase>();
+        }
+
+        /// <summary>
+        /// Activates the specified purpose.
+        /// </summary>
+        /// <param name="purpose">The purpose.</param>
+        public void Activate(ActivationPurpose purpose)
+        {
+            if (this._activator != null)
+            {
+                this._activator.Activate(purpose);
+            }
+        }
+
+        /// <summary>
+        /// Binds the specified activator.
+        /// </summary>
+        /// <param name="activator">The activator.</param>
+        public void Bind(IActivator activator)
+        {
+            if (_activator == activator) { return; }
+
+            if (activator != null && null != _activator) { throw new System.InvalidOperationException(); }
+
+            _activator = activator;
         }
     }
 }
--- Main/Solutions/Presto/Source/Common/PrestoCommon/Entities/ApplicationServer.cs
+++ Main/Solutions/Presto/Source/Common/PrestoCommon/Entities/ApplicationServer.cs
@@ -1,12 +1,18 @@
 ﻿using System.Collections.ObjectModel;
+using Db4objects.Db4o;
+using Db4objects.Db4o.Activation;
+using Db4objects.Db4o.TA;
 
 namespace PrestoCommon.Entities
 {
     /// <summary>
     /// ApplicationServer entity
     /// </summary>
-    public class ApplicationServer
+    public class ApplicationServer : IActivatable
     {
+        [Transient]
+        private IActivator _activator;
+
         private Collection<Application> _applications;
 
         /// <summary>
@@ -36,6 +42,42 @@ namespace PrestoCommon.Entities
 
                 return this._applications;
             }
+        }
+
+        /// <summary>
+        /// Returns a <see cref="System.String"/> that represents this instance.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="System.String"/> that represents this instance.
+        /// </returns>
+        public override string ToString()
+        {
+            return this.Name;
+        }
+
+        /// <summary>
+        /// Activates the specified purpose.
+        /// </summary>
+        /// <param name="purpose">The purpose.</param>
+        public void Activate(ActivationPurpose purpose)
+        {
+            if (this._activator != null)
+            {
+                this._activator.Activate(purpose);
+            }
+        }
+
+        /// <summary>
+        /// Binds the specified activator.
+        /// </summary>
+        /// <param name="activator">The activator.</param>
+        public void Bind(IActivator activator)
+        {
+            if (_activator == activator) { return; }
+
+            if (activator != null && null != _activator) { throw new System.InvalidOperationException(); }
+
+            _activator = activator;
         }
     }
 }

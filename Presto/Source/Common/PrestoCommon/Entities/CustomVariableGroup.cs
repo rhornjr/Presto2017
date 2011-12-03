b0b2b813f50daf3f5a4fabbbb657e95399958e3b
Bob Horn <rhornjr@gmail.com>
2011-12-03 22:28:53 +0000

    Add custom variables.

---
+++ Presto/Source/Common/PrestoCommon/Entities/CustomVariableGroup.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.ObjectModel;
+
+namespace PrestoCommon.Entities
+{
+    /// <summary>
+    /// Container for <see cref="CustomVariable"/>s
+    /// </summary>
+    public class CustomVariableGroup : ActivatableEntity
+    {
+        private Collection<CustomVariable> _customVariables;
+
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the custom variables.
+        /// </summary>
+        /// <value>
+        /// The custom variables.
+        /// </value>
+        public Collection<CustomVariable> CustomVariables
+        {
+            get
+            {
+                if (this._customVariables == null)
+                {
+                    this._customVariables = new Collection<CustomVariable>();
+                }
+                return this._customVariables;
+            }
+        }
+    }
+}

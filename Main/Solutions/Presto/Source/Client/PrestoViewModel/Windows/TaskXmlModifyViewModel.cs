0a7343b65ef4f96357823c2fd41b2c6368319cca
rhornjr_cp <SND\rhornjr_cp>
2011-12-08 01:16:10 +0000

    Add TaskXmlModify view and view model.

---
+++ Main/Solutions/Presto/Source/Client/PrestoViewModel/Windows/TaskXmlModifyViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System.Windows.Input;
+using PrestoCommon.Enums;
+using PrestoViewModel.Mvvm;
+
+namespace PrestoViewModel.Windows
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [TaskTypeAttribute(TaskType.XmlModify)]
+    public class TaskXmlModifyViewModel : TaskViewModel
+    {
+        /// <summary>
+        /// Gets or sets the ok command.
+        /// </summary>
+        /// <value>
+        /// The ok command.
+        /// </value>
+        public ICommand OkCommand { get; set; }
+
+        /// <summary>
+        /// Gets or sets the cancel command.
+        /// </summary>
+        /// <value>
+        /// The cancel command.
+        /// </value>
+        public ICommand CancelCommand { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TaskXmlModifyViewModel"/> class.
+        /// </summary>
+        public TaskXmlModifyViewModel()
+        {
+            if (DesignMode.IsInDesignMode) { return; }
+
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            this.OkCommand     = new RelayCommand(_ => Save());
+            this.CancelCommand = new RelayCommand(_ => Cancel());
+        }
+
+        private void Save()
+        {
+            this.Close();
+        }
+
+        private void Cancel()
+        {
+            this.UserCanceled = true;
+            this.Close();
+        }
+    }
+}

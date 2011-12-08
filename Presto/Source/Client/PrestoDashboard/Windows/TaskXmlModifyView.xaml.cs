b55b6c74ab802be83ff651dbb0f5a236c5521f18
Bob Horn <rhornjr@gmail.com>
2011-12-08 01:16:10 +0000

    Add TaskXmlModify view and view model.

---
+++ Presto/Source/Client/PrestoDashboard/Windows/TaskXmlModifyView.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using PrestoViewModel.Mvvm;
+using PrestoViewModel.Windows;
+
+namespace PrestoDashboard.Windows
+{
+    /// <summary>
+    /// Interaction logic for TaskXmlModifyView.xaml
+    /// </summary>
+    [ViewModelAttribute(typeof(TaskXmlModifyViewModel))]
+    public partial class TaskXmlModifyView : Window
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TaskXmlModifyView"/> class.
+        /// </summary>
+        public TaskXmlModifyView()
+        {
+            InitializeComponent();
+        }
+    }
+}

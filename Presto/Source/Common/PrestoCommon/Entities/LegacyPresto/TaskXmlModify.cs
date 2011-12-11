5de8d150615af13104b1467b63a564ef026e291a
Bob Horn <rhornjr@gmail.com>
2011-12-11 19:25:44 +0000

    Add legacy Presto task entities so they can be imported into the new Presto.

---
+++ Presto/Source/Common/PrestoCommon/Entities/LegacyPresto/TaskXmlModify.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace PrestoCommon.Entities.LegacyPresto
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Serializable]
+    public class TaskXmlModify : TaskBase
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        public string XmlPathAndFileName { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string NodeToChange { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string AttributeKey { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string AttributeKeyValue { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string AttributeToChange { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string AttributeToChangeValue { get; set; }
+    }
+}

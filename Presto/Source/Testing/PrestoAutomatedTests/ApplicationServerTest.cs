51e7e9821a95914360eb6273558dfaaf4701464f
Bob Horn <rhornjr@gmail.com>
2012-02-05 15:19:43 +0000

    Add TestUtility.PopulateData() to app server test.

--- Presto/Source/Testing/PrestoAutomatedTests/ApplicationServerTest.cs
+++ Presto/Source/Testing/PrestoAutomatedTests/ApplicationServerTest.cs
@@ -34,15 +34,14 @@ namespace PrestoAutomatedTests
         }
 
         #region Additional test attributes
-        // 
-        //You can use the following additional attributes as you write your tests:
-        //
+
         //Use ClassInitialize to run code before running the first test in the class
-        //[ClassInitialize()]
-        //public static void MyClassInitialize(TestContext testContext)
-        //{
-        //}
-        //
+        [ClassInitialize()]
+        public static void MyClassInitialize(TestContext testContext)
+        {
+            TestUtility.PopulateData();
+        }
+        
         //Use ClassCleanup to run code after all tests in a class have run
         //[ClassCleanup()]
         //public static void MyClassCleanup()

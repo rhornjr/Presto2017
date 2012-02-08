4924f56f03c4bbcd413b3be0582d9db38acd0032
Bob Horn <rhornjr@gmail.com>
2012-02-08 18:39:00 +0000

    Add ability for a user to initiate a ping request and have the presto task runners respond to it.

---
+++ Presto/Source/Client/PrestoDashboard/Tabs/PingView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Controls;
+
+namespace PrestoDashboard.Tabs
+{
+    /// <summary>
+    /// Interaction logic for PingView.xaml
+    /// </summary>
+    public partial class PingView : UserControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PingView"/> class.
+        /// </summary>
+        public PingView()
+        {
+            InitializeComponent();
+        }
+    }
+}

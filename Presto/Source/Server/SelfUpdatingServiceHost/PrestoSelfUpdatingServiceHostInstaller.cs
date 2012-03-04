1cf82bda37062b19b3f626abd7bb938450dba383
Bob Horn <rhornjr@gmail.com>
2012-03-04 23:55:02 +0000

    Add user interface action during installation: let the user enter the PTR binary path.

--- Presto/Source/Server/SelfUpdatingServiceHost/PrestoSelfUpdatingServiceHostInstaller.cs
+++ Presto/Source/Server/SelfUpdatingServiceHost/PrestoSelfUpdatingServiceHostInstaller.cs
@@ -1,19 +1,41 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections;
 using System.ComponentModel;
+using System.Configuration;
 using System.Configuration.Install;
-using System.Linq;
-
+using System.Globalization;
 
 namespace SelfUpdatingServiceHost
 {
     [RunInstaller(true)]
-    public partial class PrestoSelfUpdatingServiceHostInstaller : System.Configuration.Install.Installer
+    public partial class PrestoSelfUpdatingServiceHostInstaller : Installer
     {
         public PrestoSelfUpdatingServiceHostInstaller()
         {
             InitializeComponent();
+        }
+
+        public override void Install(IDictionary stateSaver)
+        {
+            base.Install(stateSaver);
+
+            // http://raquila.com/software/configure-app-config-application-settings-during-msi-install/
+
+            // This worked. I didn't have to attach to process manually. In solution explorer, I just right-clicked the
+            // installer project and chose Install. These lines below allowed me to debug here.
+            //System.Diagnostics.Debugger.Launch();
+            //System.Diagnostics.Debugger.Break();
+
+            string targetDirectory = Context.Parameters["targetdir"];
+
+            string prestoTaskRunnerBinaryPath = Context.Parameters["Param1"];            
+
+            string exePath = string.Format(CultureInfo.InvariantCulture, "{0}SelfUpdatingServiceHost.exe", targetDirectory);
+
+            Configuration config = ConfigurationManager.OpenExeConfiguration(exePath);
+
+            config.AppSettings.Settings["SourceBinaryPath"].Value = prestoTaskRunnerBinaryPath;
+
+            config.Save();
         }
     }
 }

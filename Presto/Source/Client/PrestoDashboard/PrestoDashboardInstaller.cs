f240d24378b89953be3ac84b9720efd269b4a4be
Bob Horn <rhornjr@gmail.com>
2012-03-10 15:17:04 +0000

    Finish Presto Dashboard installer. Getting close to first official 2.0 release...

--- Presto/Source/Client/PrestoDashboard/PrestoDashboardInstaller.cs
+++ Presto/Source/Client/PrestoDashboard/PrestoDashboardInstaller.cs
@@ -1,19 +1,51 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections;
 using System.ComponentModel;
-using System.Configuration.Install;
-using System.Linq;
+using System.Configuration;
+using System.Globalization;
 
 
 namespace PrestoDashboard
 {
+    /// <summary>
+    /// 
+    /// </summary>
     [RunInstaller(true)]
     public partial class PrestoDashboardInstaller : System.Configuration.Install.Installer
     {
+        /// <summary>
+        /// 
+        /// </summary>
         public PrestoDashboardInstaller()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="stateSaver"></param>
+        public override void Install(IDictionary stateSaver)
+        {
+            base.Install(stateSaver);
+
+            // http://raquila.com/software/configure-app-config-application-settings-during-msi-install/
+
+            // This worked. I didn't have to attach to process manually. In solution explorer, I just right-clicked the
+            // installer project and chose Install. These lines below allowed me to debug here.
+            //System.Diagnostics.Debugger.Launch();
+            //System.Diagnostics.Debugger.Break();
+
+            string targetDirectory = Context.Parameters["targetdir"];
+
+            string connectionString = Context.Parameters["Param1"];
+
+            string exePath = string.Format(CultureInfo.InvariantCulture, "{0}PrestoDashboard.exe", targetDirectory);
+
+            Configuration config = ConfigurationManager.OpenExeConfiguration(exePath);
+
+            config.ConnectionStrings.ConnectionStrings["RavenDb"].ConnectionString = connectionString;
+
+            config.Save();
         }
     }
 }

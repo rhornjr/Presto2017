776a1742735171af5e299ac78d79f8ff8a3203c4
rhornjr_cp <SND\rhornjr_cp>
2012-11-18 22:23:39 +0000

    Updated files for prod deployment.

--- Main/Solutions/Presto/Solution Items/SolutionInfo.cs
+++ Main/Solutions/Presto/Solution Items/SolutionInfo.cs
@@ -6,4 +6,4 @@ using System.Reflection;
 [assembly: AssemblyTrademark("")]
 
 [assembly: AssemblyVersion("2.2.0.0")]
-[assembly: AssemblyFileVersion("2.2.1.0")]
+[assembly: AssemblyFileVersion("2.2.2.0")]

06acabfaf7ce1d80347afcb98cf0f0a8cb0edc6a
Bob Horn <rhornjr@gmail.com>
2013-04-02 15:50:43 +0000

    Update version and ClickOnce deployment.

--- Presto/Solution Items/SolutionInfo.cs
+++ Presto/Solution Items/SolutionInfo.cs
@@ -6,4 +6,4 @@ using System.Reflection;
 [assembly: AssemblyTrademark("")]
 
 [assembly: AssemblyVersion("2.4.0.0")]
-[assembly: AssemblyFileVersion("2.4.3.1")]
+[assembly: AssemblyFileVersion("2.4.4.0")]

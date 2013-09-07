13f17a6a521b0e0bd84e49cac79c80c53e06552b
Bob Horn <rhornjr@gmail.com>
2013-09-07 21:45:52 +0000

    Get apps page to show real apps. Used knockout.

---
+++ Presto/Source/Client/PrestoWeb/App/dataService.js
@@ -0,0 +1,44 @@
+﻿define(['model', 'services/logger'],
+    function (model, logger) {
+
+    var getApps = function(appsObservable) {
+
+        appsObservable([]);  // reset
+
+        var options = {
+            url: '/HotTowel/GetApps',
+            type: 'POST',
+            dataType: 'json'
+        };
+
+        return $.ajax(options)
+            .then(onAppsRetrieved)
+            .fail(loadingAppsFailed);
+
+        function onAppsRetrieved(data) {
+            var apps = [];
+            data.sort(sortApps);
+            data.forEach(function (app) {
+                var knockoutApp = new model.ConvertAppToKnockoutApp(app);
+                apps.push(knockoutApp);
+            });
+            appsObservable(apps);  // Add all apps at once, so we don't get a notification for each push
+            logger.log('Loaded apps', apps, true);
+        }
+
+        function loadingAppsFailed(jqXhr, textStatus) {
+            var message = 'Apps failed to load. ' + textStatus;
+            logger.logError(message, jqXhr, true);
+        }
+
+        function sortApps(s1, s2) {
+            return (s1.name > s2.name) ? 1 : -1;
+        }
+    };
+
+    var dataService = {
+        getApps: getApps
+    };
+
+    return dataService;
+});
--- Presto/Source/Client/PrestoWeb/App/viewmodels/apps.js
+++ Presto/Source/Client/PrestoWeb/App/viewmodels/apps.js
@@ -1,15 +1,31 @@
-﻿define(['services/logger'], function (logger) {
+﻿define(['services/logger', 'dataService'], function (logger, dataService) {
+
+    var apps = ko.observableArray();
+    var initialized = false;
+
     var vm = {
         activate: activate,
-        title: 'Applications'
+        title: 'Applications',
+        apps: apps,
+        refresh: refresh
     };
 
     return vm;
 
     //#region Internal Methods
+
     function activate() {
+        if (initialized) { return; }
+
+        initialized = true;
         logger.log('Apps View Activated', null, 'apps', true);
-        return true;
+        return refresh();
+        //return true;
     }
+
+    function refresh() {
+        return dataService.getApps(apps);
+    }
+
     //#endregion
 });

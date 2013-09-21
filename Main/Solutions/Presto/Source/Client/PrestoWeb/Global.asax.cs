e581cf2c02ea7066ccdace92017fc4ecd4935310
rhornjr_cp <SND\rhornjr_cp>
2013-09-21 23:51:34 +0000

    Added new PrestoWeb project that uses jQuery Mobile instead of Hot Towel.

--- Main/Solutions/Presto/Source/Client/PrestoWeb/Global.asax.cs
+++ Main/Solutions/Presto/Source/Client/PrestoWeb/Global.asax.cs
@@ -1,4 +1,7 @@
-﻿﻿using System.Web;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -13,6 +16,9 @@ namespace PrestoWeb
     {
         protected void Application_Start()
         {
+            AreaRegistration.RegisterAllAreas();
+
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);

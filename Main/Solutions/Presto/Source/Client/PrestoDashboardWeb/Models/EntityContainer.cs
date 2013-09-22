815113762514d4acdcd54663ad3f83f9da2e459a
rhornjr_cp <SND\rhornjr_cp>
2013-09-22 15:36:38 +0000

    Pass an EntityContainer to Index.cshtml so it can pass the appropriate entities to each partial view. Show the variable groups. Order each entity list by name.

---
+++ Main/Solutions/Presto/Source/Client/PrestoDashboardWeb/Models/EntityContainer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using PrestoCommon.Entities;
+
+namespace PrestoDashboardWeb.Models
+{
+    public class EntityContainer
+    {
+        public IEnumerable<Application> Applications { get; set; }
+        public IEnumerable<ApplicationServer> Servers { get; set; }
+        public IEnumerable<CustomVariableGroup> VariableGroups { get; set; }
+    }
+}

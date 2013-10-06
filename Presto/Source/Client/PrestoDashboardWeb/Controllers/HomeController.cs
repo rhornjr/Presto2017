15c2789142564f12fc8b9b1c4df549017fccd81e
Bob Horn <rhornjr@gmail.com>
2013-10-06 17:30:18 +0000

    Working on saving a task. Not done.

--- Presto/Source/Client/PrestoDashboardWeb/Controllers/HomeController.cs
+++ Presto/Source/Client/PrestoDashboardWeb/Controllers/HomeController.cs
@@ -22,6 +22,8 @@ namespace PrestoDashboardWeb.Controllers
             //apps.Add(app3);
             //apps.Add(app4);
 
+            // ToDo: These should all be done with *one call* to the service.
+
             using (var prestoWcf = new PrestoWcf<IApplicationService>())
             {
                 container.Applications = prestoWcf.Service.GetAllApplications(true).OrderBy(x => x.Name);
@@ -52,6 +54,26 @@ namespace PrestoDashboardWeb.Controllers
 
             JsonResult jsonResult = new JsonResult();
             jsonResult.Data = app;
+            return jsonResult;
+        }
+
+        [HttpPost]
+        public ActionResult SaveTask(string app, TaskDosCommand task)  // Couldn't use TaskBase as parameter: jQuery error: Cannot create an abstract class
+        {
+            // Note: When app is of type string, the parameter is null. I believe this is because the call to this
+            //       method uses JSON.stringify() and that can't create the abstract TaskBase List<TaskBase>.
+
+            // This is my exact issue: http://stackoverflow.com/q/5861241/279516
+            // Two options:
+            //   1. Use a concrete type as action parameter
+            //   2. Write a custom model binder for this abstract class which based on some request parameters will return a specific instance.
+
+            // Another option is to NOT have an app parameter, but just take the app ID and eTag.
+
+            // Just return the task for now, to test that the call is working.
+            // Note, we really should be saving the entire app.
+            JsonResult jsonResult = new JsonResult();
+            jsonResult.Data = task;
             return jsonResult;
         }
 

fafbd47554403f2fd1ac63c059067132d5169b6d
Bob Horn <rhornjr@gmail.com>
2014-06-17 21:20:17 +0000

    Show servers.

--- Presto/Source/Client/PrestoWeb/Controllers/ServersController.cs
+++ Presto/Source/Client/PrestoWeb/Controllers/ServersController.cs
@@ -13,13 +13,13 @@ namespace PrestoWeb.Controllers
 {
     // The origin is the web server.
     [EnableCors(origins: "http://fs-6103:8048", headers: "*", methods: "*")]
-    public class AppsController : ApiController
+    public class ServersController : ApiController
     {
-        public IEnumerable<Application> Get()
+        public IEnumerable<ApplicationServer> Get()
         {
-            using (var prestoWcf = new PrestoWcf<IApplicationService>())
+            using (var prestoWcf = new PrestoWcf<IServerService>())
             {
-                return prestoWcf.Service.GetAllApplications(true).OrderBy(x => x.Name);
+                return prestoWcf.Service.GetAllServersSlim().OrderBy(x => x.Name);
             }
         }
     }
--- Presto/Source/Client/PrestoWeb/app/js/controllers.js
+++ Presto/Source/Client/PrestoWeb/app/js/controllers.js
@@ -10,4 +10,8 @@ angular.module('myApp.controllers', []).
         });
   }])
   .controller('serversController', ['$scope', '$http', function ($scope, $http) {
+      $http.get('http://fs-6103/PrestoWebApi/api/servers/')
+        .then(function (result) {
+            $scope.servers = result.data;
+        });
   }]);

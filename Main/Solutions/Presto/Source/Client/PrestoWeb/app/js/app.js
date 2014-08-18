e4ff4fde2e02172073e7c5cad8dddf454c5e5a87
rhornjr_cp <SND\rhornjr_cp>
2014-08-18 14:41:55 +0000

    And some comments and get a refresh button working.

--- Main/Solutions/Presto/Source/Client/PrestoWeb/app/js/app.js
+++ Main/Solutions/Presto/Source/Client/PrestoWeb/app/js/app.js
@@ -1,5 +1,10 @@
 'use strict';
 
+// To debug this web app, make sure PrestoWeb is the startup project, then hit the Start (Internet Explorer, etc.) button.
+// This will start the Web API and the web app. Then navigate to http://localhost/PrestoWebApi/app. This allows us to even
+// step through the JavaScript code.
+// Note: Since this is all deployed to IIS, Visual Studio does not have to be used to run the app. Just browse to the link above.
+
 // Declare app level module which depends on filters, and services
 var app = angular.module('myApp', [
   'ngAnimate',
@@ -19,8 +24,8 @@ app.factory('appsRepository', ['$http', function ($http) {
     var lastRefreshTime;
 
     return {
-        getApps: function (callbackFunction) {
-            if (data) {
+        getApps: function (forceRefresh, callbackFunction) {
+            if (data && forceRefresh == false) {
                 callbackFunction(data, lastRefreshTime);
                 return;
             }

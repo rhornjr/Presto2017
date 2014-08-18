807c5cc21ba6e33a68775179b1d7175d62dd9604
rhornjr_cp <SND\rhornjr_cp>
2014-08-18 14:56:15 +0000

    Refresh/update servers just like we do for apps.

--- Main/Solutions/Presto/Source/Client/PrestoWeb/app/js/controllers.js
+++ Main/Solutions/Presto/Source/Client/PrestoWeb/app/js/controllers.js
@@ -20,10 +20,20 @@ angular.module('myApp.controllers', []).
 
         $scope.refresh(false);
   })
-  .controller('serversController', ['$scope', '$http', function ($scope, $http) {
-      // Commented just for testing so we don't have to wait for the servers to load.
-      //$http.get('http://localhost/PrestoWebApi/api/servers/')
-      //  .then(function (result) {
-      //      $scope.servers = result.data;
-      //  });
-  }]);
+  .controller('serversController', function ($scope, serversRepository) {
+
+      $scope.refresh = function (forceRefresh) {
+          // Since the eventual $http call is async, we have to provide a callback function to use the data retrieved.
+          $scope.loading = 1;
+          $scope.servers = null;
+          serversRepository.getServers(forceRefresh, function (dataResponse, lastRefreshTime) {
+              $scope.servers = dataResponse;
+              $scope.lastRefreshTime = lastRefreshTime;
+              $scope.loading = 0;
+          });
+      };
+
+      // ----------------------------------------------------------------------------------------
+
+      $scope.refresh(false);
+  });

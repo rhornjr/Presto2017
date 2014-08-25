856185b7c318dd7cb2e61da9536461566ad3cd00
rhornjr_cp <SND\rhornjr_cp>
2014-08-25 19:54:06 +0000

    Get an app by its ID. Deal with slash in Raven ID field.

---
+++ Main/Solutions/Presto/Source/Client/PrestoWeb/Controllers/AppController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Http;
+using System.Web.Http.Cors;
+using PrestoCommon.Entities;
+using PrestoCommon.Interfaces;
+using PrestoCommon.Wcf;
+
+namespace PrestoWeb.Controllers
+{
+    // The origin is the web server.
+    [EnableCors(origins: "http://localhost:8048", headers: "*", methods: "*")]
+    public class AppController : ApiController
+    {
+        public Application Get(string id)
+        {
+            // Because RavenDB has a slash in the ID. The caller replaced it with ^^.
+            id = id.Replace("^^", "/");
+
+            using (var prestoWcf = new PrestoWcf<IApplicationService>())
+            {
+                return prestoWcf.Service.GetById(id);
+            }
+        }
+    }
+}

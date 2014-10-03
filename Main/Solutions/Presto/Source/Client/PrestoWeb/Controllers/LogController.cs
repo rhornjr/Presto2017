9934739dc77b5e98fbb9801bc1a0d142c43b3b1f
rhornjr_cp <SND\rhornjr_cp>
2014-10-03 21:12:50 +0000

    Add log page.

---
+++ Main/Solutions/Presto/Source/Client/PrestoWeb/Controllers/LogController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using PrestoCommon.Entities;
+using PrestoCommon.Interfaces;
+using PrestoCommon.Wcf;
+
+namespace PrestoWeb.Controllers
+{
+    // The origin is the web server.
+    [EnableCors(origins: "http://fs-12220", headers: "*", methods: "*")]
+    public class LogController : ApiController
+    {
+        public IEnumerable<LogMessage> Get()
+        {
+            using (var prestoWcf = new PrestoWcf<IBaseService>())
+            {
+                return prestoWcf.Service.GetMostRecentLogMessagesByCreatedTime(50);
+            }
+        }
+    }
+}
--- Main/Solutions/Presto/Source/Client/PrestoWeb/app/js/controllers.js
+++ Main/Solutions/Presto/Source/Client/PrestoWeb/app/js/controllers.js
@@ -52,6 +52,31 @@ angular.module('myApp.controllers', []).
 
       $scope.refresh(false);
   })
+  .controller('logController', function ($scope, logRepository) {
+      $scope.loading = 1;
+      $scope.logMessages = null;
+
+      $scope.gridOptions = {
+          data: 'logMessages',
+          multiSelect: false,
+          columnDefs: [{ field: 'MessageCreatedTime', displayName: 'Time', width: "20%", resizable: true },
+                       { field: 'Message', displayName: 'Message', width: "58%", resizable: true },
+                       { field: 'UserName', displayName: 'User', width: "20%", resizable: true }]
+      };
+
+      $scope.refresh = function (forceRefresh) {
+          // Since the eventual $http call is async, we have to provide a callback function to use the data retrieved.          
+          logRepository.getLogs(forceRefresh, function (dataResponse, lastRefreshTime) {
+              $scope.logMessages = dataResponse;
+              $scope.lastRefreshTime = lastRefreshTime;
+              $scope.loading = 0;
+          });
+      };
+
+      // ----------------------------------------------------------------------------------------
+
+      $scope.refresh(false);
+  })
   .controller('appController', function ($scope, $http, $routeParams) {
       $scope.appId = $routeParams.appId;
       var modifiedAppId = $scope.appId.replace("/", "^^");  // Because we shouldn't send slashes in a web API call.

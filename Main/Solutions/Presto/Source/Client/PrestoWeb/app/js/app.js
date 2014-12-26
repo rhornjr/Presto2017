998e1a559fd43b13b971ac24a8406544b5125fd4
rhornjr_cp <SND\rhornjr_cp>
2014-12-26 21:57:54 +0000

    Load a user-selected server.

--- Main/Solutions/Presto/Source/Client/PrestoWeb/app/js/app.js
+++ Main/Solutions/Presto/Source/Client/PrestoWeb/app/js/app.js
@@ -105,6 +105,7 @@ app.config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/apps', { templateUrl: 'partials/apps.html', controller: 'appsController' });
     $routeProvider.when('/servers', { templateUrl: 'partials/servers.html', controller: 'serversController' });
     $routeProvider.when('/app/:appId?', { templateUrl: 'partials/app.html', controller: 'appController' });
+    $routeProvider.when('/server/:serverId?', { templateUrl: 'partials/server.html', controller: 'serverController' });
     $routeProvider.when('/installs', { templateUrl: 'partials/installs.html', controller: 'installsController' });
     $routeProvider.when('/log', { templateUrl: 'partials/log.html', controller: 'logController' });
     $routeProvider.otherwise({ redirectTo: '/apps' });

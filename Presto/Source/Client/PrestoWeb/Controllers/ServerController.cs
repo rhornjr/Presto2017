64199b4b998405151351bf8f5472e11f1413a021
Bob Horn <rhornjr@gmail.com>
2014-12-29 22:26:50 +0000

    Allow the user to deploy an app from the server page.

--- Presto/Source/Client/PrestoWeb/Controllers/ServerController.cs
+++ Presto/Source/Client/PrestoWeb/Controllers/ServerController.cs
@@ -1,8 +1,10 @@
-﻿using System.Web.Http;
+﻿using System.Collections.Generic;
+using System.Web.Http;
 using System.Web.Http.Cors;
 using PrestoCommon.Entities;
 using PrestoCommon.Interfaces;
 using PrestoCommon.Wcf;
+using PrestoWeb.EntityContainers;
 
 namespace PrestoWeb.Controllers
 {
@@ -18,6 +20,21 @@ namespace PrestoWeb.Controllers
             using (var prestoWcf = new PrestoWcf<IServerService>())
             {
                 return prestoWcf.Service.GetServerById(id);
+            }
+        }
+
+        public void InstallApp(AppServerAndAppWithGroup appServerAndAppWithGroup)
+        {
+            var server       = appServerAndAppWithGroup.Server;
+            var appWithGroup = appServerAndAppWithGroup.AppWithGroup;
+
+            var serverForceInstallation = new ServerForceInstallation(server, appWithGroup);
+            var serverForceInstallations = new List<ServerForceInstallation>();
+            serverForceInstallations.Add(serverForceInstallation);
+
+            using (var prestoWcf = new PrestoWcf<IServerService>())
+            {
+                prestoWcf.Service.SaveForceInstallations(serverForceInstallations);
             }
         }
     }

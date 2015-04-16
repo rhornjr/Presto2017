5f04243e2600e68b53479fd6f09f1a721c17fb02
rhornjr_cp <SND\rhornjr_cp>
2015-04-16 13:39:01 +0000

    Update version for coming deployment.

--- Main/Solutions/Presto/Solution Items/SolutionInfo.cs
+++ Main/Solutions/Presto/Solution Items/SolutionInfo.cs
@@ -6,4 +6,4 @@ using System.Reflection;
 [assembly: AssemblyTrademark("")]
 
 [assembly: AssemblyVersion("3.6.0.0")]
-[assembly: AssemblyFileVersion("3.6.2.2")]
+[assembly: AssemblyFileVersion("3.6.3.0")]

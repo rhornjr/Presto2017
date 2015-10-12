e5db810e6c5cbca0dfc44fa4ba95f0815201f214
rhornjr_cp <SND\rhornjr_cp>
2015-10-12 15:21:43 +0000

    Don't email certain exceptions within the PTR.

--- Main/Solutions/Presto/Source/Server/PrestoTaskRunner/Utility.cs
+++ Main/Solutions/Presto/Source/Server/PrestoTaskRunner/Utility.cs
@@ -3,6 +3,7 @@ using System.Configuration;
 using System.Globalization;
 using PrestoCommon.Misc;
 using Xanico.Core;
+using System.Threading;
 
 namespace PrestoTaskRunner
 {
@@ -23,6 +24,14 @@ namespace PrestoTaskRunner
 
         internal static void ProcessException(Exception ex, string source = "")
         {
+            // Don't email timeout exceptions. They occur too frequently (in Malaysia) to send emails.
+            // ThreadAbortException happens when the PTR gets updated when updating the manifest file.
+            if (ex is TimeoutException || ex is ThreadAbortException)
+            {
+                CommonUtility.ProcessException(ex, source, false);  // log only
+                return;
+            }
+
             int maxExceptionEmailFrequencyInSeconds =
                 Convert.ToInt32(ConfigurationManager.AppSettings["maxExceptionEmailFrequencyInSeconds"], CultureInfo.InvariantCulture);
 

785c2edabef624d8a8bb7f8179d316da17a290e1
AlpacaNate_cp <SND\AlpacaNate_cp>
2015-12-06 22:19:02 +0000

    Show custom variables in the grid.

---
+++ Main/Solutions/Presto/Source/Client/PrestoWeb/Controllers/VariableGroupsController.cs
@@ -0,0 +1,27 @@
+﻿using PrestoCommon.Entities;
+using PrestoCommon.Interfaces;
+using PrestoCommon.Wcf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace PrestoWeb.Controllers
+{
+    // The origin is the web server.
+    [EnableCors(origins: "http://apps.firstsolar.com", headers: "*", methods: "*")]  // * See notes, below, for why
+    public class VariableGroupsController : ApiController
+    {
+        public IEnumerable<CustomVariableGroup> Get()
+        {
+            using (var prestoWcf = new PrestoWcf<ICustomVariableGroupService>())
+            {
+                var groups = prestoWcf.Service.GetAllGroups().OrderBy(x => x.Name);
+                return groups;
+            }
+        }
+    }
+}

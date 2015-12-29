ad5156874ab66abe875680a5ec87b43c110a5e9a
rhornjr_cp <SND\rhornjr_cp>
2015-12-29 13:58:51 +0000

    Add resolve page.

---
+++ Main/Solutions/Presto/Source/Client/PrestoWeb/app/js/resolve.js
@@ -0,0 +1,13 @@
+﻿(function () {
+
+    'use strict';
+
+    angular.module('myApp.controllers').controller('resolveController', resolveController);
+
+    // ------------------------------- Global Controller -------------------------------
+
+    function resolveController($scope, $http) {
+        
+    }
+
+})();

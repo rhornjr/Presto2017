e4def7fbe8f8d335592ee56f7b6967588c34457f
rhornjr_cp <SND\rhornjr_cp>
2015-12-29 20:22:22 +0000

    Get the resolve page working.

---
+++ Main/Solutions/Presto/Source/Client/PrestoWeb/Controllers/ResolveController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using PrestoCommon.DataTransferObjects;
+using PrestoCommon.Entities;
+using PrestoCommon.Interfaces;
+using PrestoCommon.Misc;
+using PrestoCommon.Wcf;
+
+namespace PrestoWeb.Controllers
+{
+    [EnableCors(origins: "http://apps.firstsolar.com", headers: "*", methods: "*")]
+    public class ResolveController : ApiController
+    {
+        /// <summary>
+        /// Returns a list of resolved variables for a given <see cref="Application"/> and <see cref="ApplicationServer"/>.
+        /// </summary>
+        [HttpPost]
+        public IEnumerable<CustomVariable> ResolveVariables(AppAndServer appAndServer)
+        {
+            var app    = appAndServer.Application;
+            var server = appAndServer.Server;
+
+            // Hydrate the app with trusted data (ie: don't use what was sent from the browser).
+            Application hydratedApp;
+            using (var prestoWcf = new PrestoWcf<IApplicationService>())
+            {
+                hydratedApp = prestoWcf.Service.GetById(app.Id);
+            }
+
+            // Hydrate the server with trusted data (ie: don't use what was sent from the browser).
+            ApplicationServer hydratedServer;
+            using (var prestoWcf = new PrestoWcf<IServerService>())
+            {
+                hydratedServer = prestoWcf.Service.GetServerById(server.Id);
+            }
+
+            var appWithOverrides = new ApplicationWithOverrideVariableGroup();
+            appWithOverrides.Application = hydratedApp;
+            appWithOverrides.CustomVariableGroups = null;  // ToDo: Need to set this. Need to add it to UI first.
+
+            return VariableGroupResolver.Resolve(appWithOverrides, hydratedServer);
+        }
+    }
+}
---
+++ Main/Solutions/Presto/Source/Common/PrestoCommon/DataTransferObjects/AppAndServer.cs
@@ -0,0 +1,11 @@
+﻿using PrestoCommon.Entities;
+
+namespace PrestoCommon.DataTransferObjects
+{
+    public class AppAndServer
+    {
+        public Application Application { get; set; }
+
+        public ApplicationServer Server { get; set; }
+    }
+}

b0ae054403d64325f26b818c3860ba06630d84a1
Bob Horn <rhornjr@gmail.com>
2015-12-30 14:29:13 +0000

    On the resolve page, show grid data that gets truncated.

--- Presto/Source/Client/PrestoWeb/app/js/resolve.js
+++ Presto/Source/Client/PrestoWeb/app/js/resolve.js
@@ -105,16 +105,27 @@
     function resolveController($scope, $rootScope, $http, $uibModal, uiGridConstants) {
         $scope.selectedApp;
         $scope.selectedServer;
-        $scope.resolvedVariables;
+        $scope.resolvedVariables = [];
+        $scope.selectedVariables = [];
 
         $scope.gridResolve = {
             data: 'resolvedVariables',
             multiSelect: false,
             enableFiltering: true,
             enableRowHeaderSelection: false, // We don't want to have to click a row header to select a row. We want to just click the row itself.
-            selectedItems: $scope.selectedApps,
+            selectedItems: $scope.selectedVariables,
             columnDefs: [{ field: 'Key', displayName: 'Variable', width: "30%", resizable: true, sort: { direction: uiGridConstants.ASC, priority: 1 }, filter: { condition: uiGridConstants.filter.CONTAINS } },
                          { field: 'Value', displayName: 'Value', width: "68%", sort: { direction: uiGridConstants.ASC, priority: 2 } }]
+        };
+
+        // Act on the row selection changing.
+        $scope.gridResolve.onRegisterApi = function (gridApi) {
+            $scope.gridApi = gridApi;
+            gridApi.selection.on.rowSelectionChanged($scope, function (row) {
+                console.log(row);  // This is a nice option. It allowed me to browse the object and discover that I wanted the entity property.
+                $scope.selectedVariables.length = 0; // Truncate/clear the array. Yes, this is how it's done.
+                $scope.selectedVariables.push(row.entity);
+            });
         };
 
         $scope.pickApp = function () {

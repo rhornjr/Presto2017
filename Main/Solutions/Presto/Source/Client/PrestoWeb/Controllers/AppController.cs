1b8c1cba5f1fbbd5906b74411739cf9eb5f001a1
rhornjr_cp <SND\rhornjr_cp>
2016-05-31 13:59:32 +0000

    Export selected tasks. Add helper class for Web API HTTP exceptions.

--- Main/Solutions/Presto/Source/Client/PrestoWeb/Controllers/AppController.cs
+++ Main/Solutions/Presto/Source/Client/PrestoWeb/Controllers/AppController.cs
@@ -1,8 +1,14 @@
-﻿using System.Web.Http;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Web.Http;
 using System.Web.Http.Cors;
 using PrestoCommon.Entities;
 using PrestoCommon.Interfaces;
 using PrestoCommon.Wcf;
+using Xanico.Core;
 
 namespace PrestoWeb.Controllers
 {
@@ -29,11 +35,25 @@ namespace PrestoWeb.Controllers
             }
         }
 
-        [AcceptVerbs("GET")]
+        [AcceptVerbs("POST")]
         [Route("api/app/getTaskExportFileContents")]
-        public string GetTaskExportFileContents()
+        public string GetTaskExportFileContents(List<TaskBase> selectedTasks)
         {
-            return "snuh";
+            try
+            {
+                using (var memoryStream = new MemoryStream())
+                {
+                    var serializer = new NetDataContractSerializer();
+                    serializer.Serialize(memoryStream, selectedTasks);
+                    var streamAsString = Encoding.UTF8.GetString(memoryStream.ToArray());
+                    return streamAsString;
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.LogException(ex);
+                throw Helper.CreateHttpResponseException(ex, "Error Exporting Tasks");
+            }
         }
     }
 }
---
+++ Main/Solutions/Presto/Source/Client/PrestoWeb/Controllers/Helper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+
+namespace PrestoWeb.Controllers
+{
+    internal static class Helper
+    {
+        internal static HttpResponseException CreateHttpResponseException(Exception ex, string reasonPhrase)
+        {
+            var response = new HttpResponseMessage(HttpStatusCode.InternalServerError)
+            {
+                Content = new StringContent(ConcatenateExceptionMessages(ex)),
+                ReasonPhrase = reasonPhrase
+            };
+
+            return new HttpResponseException(response);
+        }
+
+        private static string ConcatenateExceptionMessages(Exception ex)
+        {
+            if (ex is AggregateException)
+            {
+                return ConcatenateAggregateExceptionMessages(ex as AggregateException);
+            }
+
+            var stringBuilder = new StringBuilder();
+
+            var exception = ex;
+            do
+            {
+                stringBuilder.AppendLine(exception.Message);
+                exception = exception.InnerException;
+            }
+            while (exception != null);
+
+            return stringBuilder.ToString();
+        }
+
+        private static string ConcatenateAggregateExceptionMessages(AggregateException ex)
+        {
+            var stringBuilder = new StringBuilder();
+
+            foreach (var inner in ex.InnerExceptions)
+            {
+                stringBuilder.AppendLine(inner.Message);
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}

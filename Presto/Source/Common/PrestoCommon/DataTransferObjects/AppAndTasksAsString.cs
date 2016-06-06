c14a7b6f8c86b3f28fdb23e9b975fc771bb20512
Bob Horn <rhornjr@gmail.com>
2016-06-06 19:04:17 +0000

    Import tasks on the Web API side.

---
+++ Presto/Source/Common/PrestoCommon/DataTransferObjects/AppAndTasksAsString.cs
@@ -0,0 +1,10 @@
+﻿using PrestoCommon.Entities;
+
+namespace PrestoCommon.DataTransferObjects
+{
+    public class AppAndTasksAsString
+    {
+        public Application Application { get; set; }
+        public string TasksAsString { get; set; }
+    }
+}

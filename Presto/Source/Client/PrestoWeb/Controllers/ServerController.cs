2517cc2335cf4c6baae1146fa37b86bacd06f233
Bob Horn <rhornjr@gmail.com>
2016-06-10 19:37:13 +0000

    Call the Web API to save a server. Add some exception handling.

--- Presto/Source/Client/PrestoWeb/Controllers/ServerController.cs
+++ Presto/Source/Client/PrestoWeb/Controllers/ServerController.cs
@@ -1,10 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Web.Http;
 using System.Web.Http.Cors;
 using PrestoCommon.Entities;
 using PrestoCommon.Interfaces;
 using PrestoCommon.Wcf;
 using PrestoWeb.EntityContainers;
+using Xanico.Core;
 
 namespace PrestoWeb.Controllers
 {
@@ -17,24 +19,58 @@ namespace PrestoWeb.Controllers
             // Because RavenDB has a slash in the ID. The caller replaced it with ^^.
             id = id.Replace("^^", "/");
 
-            using (var prestoWcf = new PrestoWcf<IServerService>())
+            try
             {
-                return prestoWcf.Service.GetServerById(id);
+                using (var prestoWcf = new PrestoWcf<IServerService>())
+                {
+                    return prestoWcf.Service.GetServerById(id);
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.LogException(ex);
+                throw Helper.CreateHttpResponseException(ex, "Error Getting Server");
             }
         }
 
         public void InstallApp(AppServerAndAppWithGroup appServerAndAppWithGroup)
         {
-            var server       = appServerAndAppWithGroup.Server;
-            var appWithGroup = appServerAndAppWithGroup.AppWithGroup;
-
-            var serverForceInstallation = new ServerForceInstallation(server, appWithGroup);
-            var serverForceInstallations = new List<ServerForceInstallation>();
-            serverForceInstallations.Add(serverForceInstallation);
-
-            using (var prestoWcf = new PrestoWcf<IServerService>())
+            try
             {
-                prestoWcf.Service.SaveForceInstallations(serverForceInstallations);
+                var server = appServerAndAppWithGroup.Server;
+                var appWithGroup = appServerAndAppWithGroup.AppWithGroup;
+
+                var serverForceInstallation = new ServerForceInstallation(server, appWithGroup);
+                var serverForceInstallations = new List<ServerForceInstallation>();
+                serverForceInstallations.Add(serverForceInstallation);
+
+                using (var prestoWcf = new PrestoWcf<IServerService>())
+                {
+                    prestoWcf.Service.SaveForceInstallations(serverForceInstallations);
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.LogException(ex);
+                throw Helper.CreateHttpResponseException(ex, "Error Installing App");
+            }
+        }
+
+        [AcceptVerbs("POST")]
+        [Route("api/server/save")]
+        public ApplicationServer Save(ApplicationServer server)
+        {
+            try
+            {
+                using (var prestoWcf = new PrestoWcf<IServerService>())
+                {
+                    return prestoWcf.Service.SaveServer(server);
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.LogException(ex);
+                throw Helper.CreateHttpResponseException(ex, "Error Saving Server");
             }
         }
     }

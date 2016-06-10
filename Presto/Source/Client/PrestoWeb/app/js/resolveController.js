45f77634bdfe02c8035b390a5804f2ed63be97e2
Bob Horn <rhornjr@gmail.com>
2016-06-10 18:54:56 +0000

    Add custom variable groups to an app. Move that controller code to its own controller.

--- Presto/Source/Client/PrestoWeb/app/js/resolveController.js
+++ Presto/Source/Client/PrestoWeb/app/js/resolveController.js
@@ -100,53 +100,6 @@
         $scope.refresh(true);
     });
 
-    // VARIABLE GROUPS PICKER
-
-    angular.module('myApp.controllers').controller('groupsPickerModalController', function ($scope, $uibModalInstance, uiGridConstants, variableGroupsRepository) {
-        $scope.loading = 1;
-        $scope.groups = null;
-        //$scope.selectedGroups = [];
-
-        $scope.gridGroups = {
-            data: 'groups',
-            multiSelect: true,
-            enableFiltering: true,
-            enableRowHeaderSelection: false, // We don't want to have to click a row header to select a row. We want to just click the row itself.
-            selectedItems: $scope.selectedApps,
-            columnDefs: [{ field: 'Name', displayName: 'Variable Group', width: "68%", resizable: true, sort: { direction: uiGridConstants.ASC, priority: 1 }, filter: { condition: uiGridConstants.filter.CONTAINS } }]
-        };
-
-        $scope.refresh = function (forceRefresh) {
-            $scope.loading = 1;
-            // Since the eventual $http call is async, we have to provide a callback function to use the data retrieved.
-            variableGroupsRepository.getVariableGroups(forceRefresh, function (dataResponse) {
-                $scope.groups = dataResponse;
-                $scope.loading = 0;
-            });
-        };
-
-        // Act on the row selection changing.
-        $scope.gridGroups.onRegisterApi = function (gridApi) {
-            $scope.gridApi = gridApi;
-            //gridApi.selection.on.rowSelectionChanged($scope, function (row) {
-            //    console.log(row.entity);  // This is a nice option. It allowed me to browse the object and discover that I wanted the entity property.
-            //    $scope.selectedGroups.length = 0; // Truncate/clear the array. Yes, this is how it's done.
-            //    $scope.selectedGroups.push(row.entity);
-            //});
-        };
-
-        $scope.ok = function () {
-            //$uibModalInstance.close($scope.selectedGroups);
-            $uibModalInstance.close($scope.gridApi.selection.getSelectedRows());
-        };
-
-        $scope.cancel = function () {
-            $uibModalInstance.dismiss();
-        };
-
-        $scope.refresh(true);
-    });
-
     // ------------------------------- Resolve Controller -------------------------------
 
     function resolveController($scope, $rootScope, $http, resolveRepository, $uibModal, uiGridConstants) {

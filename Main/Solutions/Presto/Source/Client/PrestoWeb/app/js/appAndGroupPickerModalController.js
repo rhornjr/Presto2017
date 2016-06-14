6ee66b2e2181b6534354040b59799ae7e97e2579
rhornjr_cp <SND\rhornjr_cp>
2016-06-14 20:48:50 +0000

    Select app and groups from server page.

---
+++ Main/Solutions/Presto/Source/Client/PrestoWeb/app/js/appAndGroupPickerModalController.js
@@ -0,0 +1,59 @@
+﻿(function () {
+
+    'use strict';
+
+    angular.module('myApp.controllers').controller('appAndGroupPickerModalController', appAndGroupPickerModalController);
+
+    function appAndGroupPickerModalController($scope, $uibModalInstance, $uibModal) {
+        $scope.pickApp = function () {
+            var modalInstance = $uibModal.open({
+                templateUrl: 'partials/appPicker.html',
+                controller: 'appPickerModalController',
+                size: 'sm',
+                windowClass: 'app-modal-window'
+            });
+
+            modalInstance.result.then(function (app) {
+                console.log("App picked", app);
+                $scope.selectedApp = app;
+            }, function () {
+                // modal dismissed
+            });
+        }
+
+        // ---------------------------------------------------------------------------------------------------
+
+        $scope.pickOverrides = function () {
+            var modalInstance = $uibModal.open({
+                templateUrl: 'partials/variableGroupsPicker.html',
+                controller: 'groupsPickerModalController',
+                size: 'sm',
+                windowClass: 'app-modal-window'
+            });
+
+            modalInstance.result.then(function (overrides) {
+                console.log("Group(s) picked", overrides);
+                $scope.selectedOverrides = overrides;
+                // Show the list of names to the user.
+                $scope.selectedOverridesNames = '';
+                for (var i = 0; i < overrides.length; i++) {
+                    $scope.selectedOverridesNames += overrides[i].Name + " | ";
+                }
+            }, function () {
+                // modal dismissed
+            });
+        }
+
+        // ---------------------------------------------------------------------------------------------------
+
+        $scope.ok = function () {
+            $uibModalInstance.close({app: $scope.selectedApp, groups: $scope.selectedOverrides});
+        };
+
+        // ---------------------------------------------------------------------------------------------------
+
+        $scope.cancel = function () {
+            $uibModalInstance.dismiss();
+        };
+    };
+})();

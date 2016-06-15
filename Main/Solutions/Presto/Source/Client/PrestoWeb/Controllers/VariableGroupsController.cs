0eb84b772676149205840f65364987075f91a031
rhornjr_cp <SND\rhornjr_cp>
2016-06-15 20:28:46 +0000

    Set up the pages and controllers to modify variable groups.

--- Main/Solutions/Presto/Source/Client/PrestoWeb/Controllers/VariableGroupsController.cs
+++ Main/Solutions/Presto/Source/Client/PrestoWeb/Controllers/VariableGroupsController.cs
@@ -1,13 +1,12 @@
-﻿using PrestoCommon.Entities;
-using PrestoCommon.Interfaces;
-using PrestoCommon.Wcf;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Cors;
+using PrestoCommon.Entities;
+using PrestoCommon.Interfaces;
+using PrestoCommon.Wcf;
+using Xanico.Core;
 
 namespace PrestoWeb.Controllers
 {
@@ -15,12 +14,40 @@ namespace PrestoWeb.Controllers
     [EnableCors(origins: "http://apps.firstsolar.com", headers: "*", methods: "*")]  // * See notes, below, for why
     public class VariableGroupsController : ApiController
     {
+        //[Route("api/groups/getGroup")]
+        public CustomVariableGroup Get(string id)
+        {
+            try
+            {
+                // Because RavenDB has a slash in the ID. The caller replaced it with ^^.
+                id = id.Replace("^^", "/");
+
+                using (var prestoWcf = new PrestoWcf<ICustomVariableGroupService>())
+                {
+                    return prestoWcf.Service.GetById(id);
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.LogException(ex);
+                throw Helper.CreateHttpResponseException(ex, "Error Getting Variable Group");
+            }
+        }
+
         public IEnumerable<CustomVariableGroup> Get()
         {
-            using (var prestoWcf = new PrestoWcf<ICustomVariableGroupService>())
+            try
             {
-                var groups = prestoWcf.Service.GetAllGroups().OrderBy(x => x.Name);
-                return groups;
+                using (var prestoWcf = new PrestoWcf<ICustomVariableGroupService>())
+                {
+                    var groups = prestoWcf.Service.GetAllGroups().OrderBy(x => x.Name);
+                    return groups;
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.LogException(ex);
+                throw Helper.CreateHttpResponseException(ex, "Error Getting Variable Groups");
             }
         }
     }
---
+++ Main/Solutions/Presto/Source/Client/PrestoWeb/app/js/variableGroupController.js
@@ -0,0 +1,24 @@
+﻿(function () {
+
+    'use strict';
+
+    angular.module('myApp.controllers').controller('variableGroupController', variableGroupController);
+
+    // ------------------------------- Variable Group Controller -------------------------------
+
+    function variableGroupController($scope, $rootScope, $http, $routeParams, uiGridConstants, showInfoModal) {
+        $scope.group = {};
+
+        // ---------------------------------------------------------------------------------------------------
+
+        $http.get('/PrestoWeb/api/variableGroups/' + $routeParams.groupId)
+            .then(function (response) {
+                $scope.group = response.data;
+                $scope.loading = 0;
+            },
+            function (response) {
+                $scope.loading = 0;
+                showInfoModal.show(response.statusText, response.data);
+            });
+    }
+})();

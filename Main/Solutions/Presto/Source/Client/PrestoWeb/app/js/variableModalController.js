05e8d174235f2f5865535400dae8783d82ec239e
rhornjr_cp <SND\rhornjr_cp>
2016-06-16 15:03:55 +0000

    Add variable modal.

---
+++ Main/Solutions/Presto/Source/Client/PrestoWeb/app/js/variableModalController.js
@@ -0,0 +1,18 @@
+﻿(function () {
+
+    'use strict';
+
+    angular.module('myApp.controllers').controller('variableModalController', variableModalController);
+
+    function variableModalController($scope, $uibModalInstance, variable) {
+        $scope.variable = variable;
+
+        $scope.ok = function () {
+            $uibModalInstance.close(variable);
+        };
+
+        $scope.cancel = function () {
+            $uibModalInstance.dismiss();
+        };
+    };
+})();

4c1cfe5130f3efe928cbf0030b40a1e9a1c9d9dd
Bob Horn <rhornjr@gmail.com>
2016-08-09 13:42:13 +0000

    Encrypt a variable's value and allow user to clear it after encrypted.

--- Presto/Source/Client/PrestoWeb/app/js/variableModalController.js
+++ Presto/Source/Client/PrestoWeb/app/js/variableModalController.js
@@ -4,15 +4,56 @@
 
     angular.module('myApp.controllers').controller('variableModalController', variableModalController);
 
-    function variableModalController($scope, $uibModalInstance, variable) {
+    function variableModalController($http, $scope, $uibModalInstance, showInfoModal, variable) {
         $scope.variable = variable;
+
+        // In case the user cancels after making changes:
+        $scope.initialKey = variable.Key;
+        $scope.initialValue = variable.Value;
+        $scope.initialValueIsEncrypted = variable.ValueIsEncrypted;
+
+        // -----------------------------------------------------------------------------
 
         $scope.ok = function () {
             $uibModalInstance.close($scope.variable);
         };
 
+        // -----------------------------------------------------------------------------
+
         $scope.cancel = function () {
+            // Reset the variable to its initial state and close.
+            $scope.variable.Key = $scope.initialKey;
+            $scope.variable.Value = $scope.initialValue;
+            $scope.variable.ValueIsEncrypted = $scope.initialValueIsEncrypted;
             $uibModalInstance.dismiss();
         };
+
+        // -----------------------------------------------------------------------------
+
+        $scope.clearValue = function () {
+            $scope.variable.Value = '';
+            $scope.variable.ValueIsEncrypted = false;
+        }
+
+        // -----------------------------------------------------------------------------
+
+        $scope.encryptValue = function () {
+            var config = {
+                url: '/PrestoWeb/api/variableGroups/encrypt/?valueToEncrypt=' + $scope.variable.Value,
+                method: 'GET'
+            };
+
+            $scope.loading = 1;
+            $http(config)
+                .then(function (result) {
+                    $scope.variable.Value = result.data;
+                    $scope.variable.ValueIsEncrypted = true;
+                    $scope.loading = 0;
+                }, function (response) {
+                    $scope.loading = 0;
+                    console.log(response);
+                    showInfoModal.show(response.statusText, response.data);
+                });
+        }
     };
 })();

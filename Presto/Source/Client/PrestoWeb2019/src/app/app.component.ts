dc62afc576a096d1e165cb3a852316c385db4f89
Robert Horn <rhornjr@gmail.com>
2019-09-10 11:28:34 -0400

    Programmatically collapse the navbar when clicking a navbar item.

--- Presto/Source/Client/PrestoWeb2019/src/app/app.component.ts
+++ Presto/Source/Client/PrestoWeb2019/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    // When we click a navbar item, collapse the navbar:
+    const navbarItems = document.querySelectorAll('.navbar-nav>li');
+    navbarItems.forEach(navbarItem => {
+      navbarItem.addEventListener('click', () => {
+        const navbar = document.querySelector('.navbar-collapse');
+        navbar.classList.remove('show');
+      })
+    });
+  }
   title = 'PrestoWeb2019';
 }
